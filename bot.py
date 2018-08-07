c47a13df0afee84da21405a4d0121d4ea3d432ef
Marvy <marvelrenju1@gmail.com>
2018-08-07 21:09:32 +0100

    First Steps

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 
+#The import os and token are setup for Heroku if you want to host locally you can just remove the import os and set "TOKEN" to your bot's token
 TOKEN = os.environ.get('TOKEN')
 
 client = commands.Bot(command_prefix = ";hs;")
@@ -9,5 +10,10 @@ client = commands.Bot(command_prefix = ";hs;")
 @client.event
 async def on_ready():
     print ("Bot is ready")
+    print('Logged in as')
+    print(client.user.name)
+    print(clent.user.id)
+    print('------')
+
 
 client.run(TOKEN)
