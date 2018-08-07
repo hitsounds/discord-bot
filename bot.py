1640e07cd5016f8c7ac3001902522aaf40f6f8be
Marvy <marvelrenju1@gmail.com>
2018-08-07 17:36:22 +0100

    ss

---
+++ bot.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+from boto.s3.connection import S3Connection
+
+TOKEN = S3Connection(os.environ['TOKEN']
+
+client = commands.Bot(command_prefix = ";hs;")
+
+@client.event
+async def on_ready():
+    print ("Bot is ready")
+
+client.run(TOKEN)
