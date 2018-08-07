907b32b847819709b867e7f967cd60583ca3cea9
Marvy <marvelrenju1@gmail.com>
2018-08-07 22:57:05 +0100

    test

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 #The import os and token are setup for Heroku if you want to host locally you can just remove the import os and set "TOKEN" to your bot's token
 TOKEN = os.environ.get('TOKEN')
 
-client = commands.Bot(command_prefix = ";hs;")
+client = commands.Bot(command_prefix = ";")
 
 @client.event
 async def on_ready():
@@ -14,6 +14,11 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+
+@client.command()
+async def me(ctx):
+    await client.say(str(ctx))
+
 
 
 client.run(TOKEN)
