508a05d031c83a3e456f6899bfeba256e51aa208
Marvy <marvelrenju1@gmail.com>
2018-08-07 19:38:05 +0100

    r

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from boto.s3.connection import S3Connection
 
-TOKEN = S3Connection(os.environ['TOKEN'])
+TOKEN = "NDc2MzgzMzQ4OTY5OTYzNTMx.Dkt04A.domVQsP2bNT9by9m17dQIbC_6wo"  #S3Connection(os.environ['TOKEN'])
 
 client = commands.Bot(command_prefix = ";hs;")
 
