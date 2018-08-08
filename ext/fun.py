34ee9a4fb2f5a8cc30bb28584f80a065ce972b11
Marvy <marvelrenju1@gmail.com>
2018-08-08 23:25:18 +0100

    test

--- ext/fun.py
+++ ext/fun.py
@@ -5,11 +5,13 @@ import random
 class fun:
     def __init__(self, client):
         self.client = client
-        self.bw = self.client.logs_from("320320664085069824", limit=10)
+        self.bw = await self.client.logs_from("320320664085069824", limit=10)
 
     @commands.command(pass_context=True)
     async def bws(self, ctx):
-        await self.client.send_message(ctx.message.channel, self.bw(random.randint(1,5500)))
+        async for self.message in self.bw:
+            await self.client.send_message(ctx.message.channel, self.message.content)
+#        await self.client.send_message(ctx.message.channel, self.bw(random.randint(1,5500)))
 
     @commands.command()
     async def ping():
