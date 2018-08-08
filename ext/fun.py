e8861316ad45028b0e1db71faddb354baf80db16
Marvy <marvelrenju1@gmail.com>
2018-08-08 22:58:46 +0100

    fix

--- ext/fun.py
+++ ext/fun.py
@@ -5,11 +5,16 @@ import random
 class fun:
     def __init__(self, client):
         self.client = client
-        self.bws = client.logs_from("320320664085069824")
+        self.bw = client.logs_from("320320664085069824")
 
     @commands.command(pass_context=True)
     async def bws(self, ctx):
-        await self.client.send_message(ctx.message.channel, random.choice(self.bws))
+        await self.client.send_message(ctx.message.channel, random.choice(self.bw))
+
+    @commands.command()
+    async def ping():
+        await self.client.say("Pong!")
+
 
 def setup(client):
     client.add_cog(fun(client))
