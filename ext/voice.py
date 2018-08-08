f484baf8b7717103936a0851b8539abeac8ddda1
Marvy <marvelrenju1@gmail.com>
2018-08-08 21:43:32 +0100

    youtube

--- ext/voice.py
+++ ext/voice.py
@@ -1,9 +1,11 @@
 import discord
+import youtube_dl
 from discord.ext import commands
 
 class voice:
     def __init__(self, client):
         self.client = client
+        self.players = {}
 
     @commands.command(pass_context=True)
     async def join(self, ctx):
@@ -14,12 +16,20 @@ class voice:
     async def leave(self, ctx):
         self.server = ctx.message.server
         self.voice_client = self.client.voice_client_in(self.server)
-        await self.voice_client.disconnect()    
+        await self.voice_client.disconnect()
 
-def setup(client):#
+    @commands.command(pass_context=True)
+    async def play(self, ctx, url):
+        self.server = ctx.message.server
+        self.voice_client = self.client.voice_client_in(self.server)
+        self.player = await self.voice_client.create_ytdl_player(url)
+        self.players[self.server.id] = self.player
+        self.player.start()
+
+
+def setup(client):
     discord.opus.load_opus("vendor/lib/libopus.so.0")
     if discord.opus.is_loaded():
-        print("opus")
-    else:
-        print("no opus")
+        print("Opus loaded!")
+
     client.add_cog(voice(client))
