2edb51ec22fd70f95c64c3c2768aba32462d54d9
Marvy <marvelrenju1@gmail.com>
2018-08-09 00:22:08 +0100

    bwl

--- ext/fun.py
+++ ext/fun.py
@@ -5,14 +5,14 @@ import random
 class fun:
     def __init__(self, client):
         self.client = client
-        self.bws = []
+        self.bwl = []
 
     
     @commands.command(pass_context=True)
     async def bws(self, ctx):
         async for self.message in self.client.logs_from(self.client.get_channel("320320664085069824"), limit=5500):
-            self.bws.append(self.message.clean_content)
-        await self.client.send_message(ctx.message.channel, random.choice(self.bws))
+            self.bwl.append(self.message.clean_content)
+        await self.client.send_message(ctx.message.channel, random.choice(self.bwl))
 
             
 #        await self.client.send_message(ctx.message.channel, self.bw(random.randint(1,5500)))
