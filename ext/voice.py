4f46b22e820a4cd6cf5bed843888961ad18bb0b3
Marvy <marvelrenju1@gmail.com>
2018-08-08 21:59:27 +0100

    pasue, resume & stop

--- ext/voice.py
+++ ext/voice.py
@@ -18,14 +18,28 @@ class voice:
         self.voice_client = self.client.voice_client_in(self.server)
         await self.voice_client.disconnect()
 
+#---------------------------------------------YOUTUBE---------------------------------------------------------------------------------
     @commands.command(pass_context=True)
     async def play(self, ctx, url):
         self.server = ctx.message.server
         self.voice_client = self.client.voice_client_in(self.server)
+        if self.voice_client == None:
+            self.voice_client = await self.client.join_voice_channel(ctx.message.author.voice.voice_channel)    
         self.player = await self.voice_client.create_ytdl_player(url)
         self.players[self.server.id] = self.player
         self.player.start()
+    
+    @commands.command(pass_context=True)
+    async def pause(ctx):
+        self.players[ctx.message.server.id].pause()
 
+    @commands.command(pass_context=True)
+    async def resume(ctx):
+        self.players[ctx.message.server.id].resume()
+
+    @commands.command(pass_context=True)
+    async def stop(ctx):
+        self.players[ctx.message.server.id].stop()
 
 def setup(client):
     discord.opus.load_opus("vendor/lib/libopus.so.0")
