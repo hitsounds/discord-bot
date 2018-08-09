2aedfb7dcb030c71e7fd7efa783dd0cfefafd413
Marvy <marvelrenju1@gmail.com>
2018-08-09 21:53:33 +0100

    member id

--- ext/database.py
+++ ext/database.py
@@ -12,7 +12,6 @@ class database:
     async def scan(self, ctx):
         await self.load()
         self.cur = self.conn.cursor()
-
         for member in ctx.message.server.members:
            print(member.id)
         self.cur.close()
