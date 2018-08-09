37d2b190020eee2e1fc54e892dc6a83dcbebf0bb
Marvy <marvelrenju1@gmail.com>
2018-08-09 13:43:22 +0100

    changes

--- ext/database.py
+++ ext/database.py
@@ -7,14 +7,14 @@ class database:
     def __init__(self, client):
         self.client = client
         self.conn = psycopg2.connect(os.environ["DATABASE_URL"], sslmode="require")
-        self.cur = self.conn.cursor()
 
     @commands.command()
     async def tablec(self):
-        self.cur.execute("""CREATE TABLE users(
- user_id bigint PRIMARY KEY,
- user_name varchar(35) 
-);""")
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE users(user_id bigint PRIMARY KEY, user_name varchar(35));")
+        self.conn.commit()
+        print ("Users table created :D")
+        self.cur.close()
 
 
 
