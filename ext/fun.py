b6f3428eb39b6ba96bacdfcc1a4024b83d5e80bb
Marvy <marvelrenju1@gmail.com>
2018-08-10 22:08:24 +0100

    clean up vars and set-up for osu

--- ext/fun.py
+++ ext/fun.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import random
 import praw
 import os
+import aiohttp
 
 class fun:
     def __init__(self, client):
@@ -10,29 +11,42 @@ class fun:
         self.reddit = praw.Reddit(client_id=os.environ.get('C_ID'),
                      client_secret=os.environ.get('C_S'),
                      user_agent='bot.py A discord bot')
+        
 
 
     
     @commands.group(pass_context=True)
     async def bws(self, ctx):
         if ctx.invoked_subcommand is None:
-            self.bwl = self.reddit.subreddit('awwnime').hot()
-            self.post_to_pick = random.randint(1, 10)
-            for i in range(0, self.post_to_pick):
-                self.submission = next(x for x in self.bwl if not x.stickied)
-            await self.client.send_message(ctx.message.channel ,self.submission.url)
+            bwl = self.reddit.subreddit('awwnime').hot()
+            post_to_pick = random.randint(1, 10)
+            for i in range(0, post_to_pick):
+                submission = next(x for x in bwl if not x.stickied)
+            await self.client.send_message(ctx.message.channel ,submission.url)
+            bwl, post_to_pick, submission = None, None, None
     
     @bws.command(pass_context=True)
     async def dump(self, ctx):
-        self.sreddit = self.reddit.subreddit('awwnime')
-        self.bwl = self.reddit.subreddit('awwnime').hot()
-        self.embed=discord.Embed(title="Current bws selection", url="https://www.reddit.com/r/awwnime/hot/")
-        self.embed.set_author(name="Source", url="https://www.reddit.com/r/awwnime/", icon_url=self.sreddit.icon_img)
-        self.embed.set_thumbnail(url="https://cdn.awwni.me/13dgm.png")
+        sreddit = self.reddit.subreddit('awwnime')
+        bwl = self.reddit.subreddit('awwnime').hot()
+        embed=discord.Embed(title="Current bws selection", url="https://www.reddit.com/r/awwnime/hot/")
+        embed.set_author(name="Source", url="https://www.reddit.com/r/awwnime/", icon_url=sreddit.icon_img)
+        embed.set_thumbnail(url="https://cdn.awwni.me/13dgm.png")
         for i in range(0, 10):
-            self.embed.add_field(name="#{}".format(i+1), value="[{url}]({url})".format(url = next(x for x in self.bwl if not x.stickied).url), inline=False)
-        await self.client.say(embed=self.embed)
-#            await self.client.send_message(ctx.message.channel ,next(x for x in self.bwl if not x.stickied).url)
+            embed.add_field(name="#{}".format(i+1), value="[{url}]({url})".format(url = next(x for x in bwl if not x.stickied).url), inline=False)
+        await self.client.say(embed=embed)
+        embed, sreddit, bwl = None, None, None
+
+
+    @commands.group(pass_context=True)
+    async def osu(self, ctx, arg):
+        if ctx.invoked_subcommand is None:
+            
+            
+
+
+
+
         
 
 
