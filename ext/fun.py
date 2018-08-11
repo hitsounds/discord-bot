4ba008a4dffc05d64eb009b66fce7e7f0312d564
Marvy <marvelrenju1@gmail.com>
2018-08-11 12:53:31 +0100

    hah

--- ext/fun.py
+++ ext/fun.py
@@ -43,7 +43,8 @@ class fun:
             session = aiohttp.ClientSession()
             resp = await session.get("https://osu.ppy.sh/api/get_user?k={key}&u={name}&m=0".format(key = self.osuAPIkey, name = arg))
             session.close()
-            await self.client.say(await resp.json()[0])
+            dtls = await resp.json()
+            await self.client.say(dtls["pp_raw"])
             session, resp = None, None
 
             
