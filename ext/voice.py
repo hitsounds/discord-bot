62dbe1be9e9db9b0ef4339ff26f5f3d341305703
Marvy <marvelrenju1@gmail.com>
2018-08-12 12:12:43 +0100

    output the mp3

--- ext/voice.py
+++ ext/voice.py
@@ -52,7 +52,7 @@ class voice:
         process.wait()
 #        await asyncio.create_subprocess_exec(["youtube-dl", "--extract-audio", "--audio-format", "mp3", "-o", "output.%(ext)s", url])
         session = aiohttp.ClientSession()
-        files = {'filedata': open("bot.py", "rb")}
+        files = {'filedata': open("output.mp3", "rb")}
         resp = await session.post('https://transfer.sh/', data=files)
         data = await resp.text()
         print(data)
