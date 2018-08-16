2ed2a68ce1c76b6e90a1c46fadc9bfa6aecb931c
Marvy <marvelrenju1@gmail.com>
2018-08-17 00:01:17 +0100

    im trying my best

--- ext/fun.py
+++ ext/fun.py
@@ -5,7 +5,6 @@ import praw
 import os
 import aiohttp
 import psycopg2
-from ext.database import database
 
 class fun:
     def __init__(self, client):
@@ -58,7 +57,7 @@ class fun:
             msg = await self.client.say("Processing")
             session = aiohttp.ClientSession()
             if arg == None:
-                conn = await database.load()
+                conn = psycopg2.connect(os.environ["DATABASE_URL"], sslmode="require")
                 cur = conn.cursor()
                 cur.execute(f"SELECT osu_id FROM users WHERE user_id = {ctx.message.author.id}")
                 arg = cur.fetchone()
