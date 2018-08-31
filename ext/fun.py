38142b3a2b7a712099f887b7063bbf3113dd48b8
Marvy <marvelrenju1@gmail.com>
2018-08-31 14:51:15 +0100

    awau

--- ext/fun.py
+++ ext/fun.py
@@ -61,7 +61,7 @@ class fun:
         self.jokes = open("georgejokes.txt", "w")
         session = aiohttp.ClientSession()
         resp = await session.get("https://docs.google.com/document/export?format=txt&id=1nzdBhs6K1aWP5VpQlcCOX7do-9ZxoCoCPMSWCtXG6m4")
-        jk = await await resp.text()
+        jk = await resp.text()
         print(jk)
         self.jokes.write(jk)
         session.close()
