76b783eb0cde348339020b4eceaada6042e5a8e0
Marv Renju <marvelrenju1@gmail.com>
2018-09-02 23:29:22 +0100

    Hitcircles bot (#13)
    * Remove space
    
    * Ytdl changes
    
    * Prep for database use & cleanup
    
    * codefactor advice
    
    * Update README.md
    
    * Higher quality audio for YTDL
    
    ;ytdl command now downloads the highest possible audio quality
    
    * Database integration
    
    If user has a id stored and does provide a name or id the bot will use the stored id. Hopefully.
    
    * Testing
    
    testing
    
    * Default value
    
    * selfless sacrifice
    
    * await waits for no man
    
    * im trying my best
    
    * Revert "im trying my best"
    
    This reverts commit 2ed2a68ce1c76b6e90a1c46fadc9bfa6aecb931c.
    
    * Revert "await waits for no man"
    
    This reverts commit 61c823d5e83ea3f62a2dc187ddbe1b4bc3265a76.
    
    * woah
    
    * I don't want to see the print
    
    * Change for the sake of change
    
    no other reason
    
    * Rounding
    
    * nice processing msg you have there
    
    * Allow users to set osu id
    
    * try s
    
    * Subcommand to nested ifs
    
    ugly but works
    
    * sleep less and await more
    
    * small changes
    
    * testing options
    
    * shorten code for future
    
    * testing
    
    * if u work
    
    il give u a cookie
    
    * nothing ever works out
    
    * nvr gonna work
    
    * YOU WON'T WIN
    
    so give up
    
    * OK u might win
    
    im sorry
    
    * i see
    
    the else is wrong is it!
    
    * most useful error msg 2020
    
    * testin
    
    * i won time to clean up
    
    * mp4?
    
    * mabi fix
    
    * i feel it
    
    * fswD
    
    * Revert "fswD"
    
    This reverts commit 5949202a4cb303a3244620d7d8247903f93730b5.
    
    * ;p
    
    * whoops
    
    * stop streaming!
    
    * file name
    
    * bigger nums
    
    * travis
    
    * google drive joke heist
    
    * fix?
    
    * lol async
    
    * incomplete fun
    
    * no await
    
    * quotes
    
    * lolololol
    
    * test
    
    * awau
    
    * my job here is done
    
    * forgot one thing
    
    my bad
    
    * credit where credit is due
    
    * hmmm
    
    * lololo
    
    * lo?
    
    * hm
    
    * session.close()
    
    * dwq
    
    * clean-up
    
    * g
    
    * Very minor changes
    
    *Removed unused code
    *Added a notification for when bot is online
    
    * Major Changes
    
    *Use one client session for all HTTP requests to improve speed
    *Alter sendFile funtion to be more useful in future code
    
    * I made a dumb dumb
    
    * push
    
    when i want to
    pull
    when i dont
    
    * Processing
    
    * tears flow when
    
    it doesn't work
    
    * sessioms
    
    * wait up
    
    * One session to rule them all
    
    * Revert "One session to rule them all"
    
    This reverts commit c146315a6cb661f0d89020ab37c8f7a82a6f12a2.
    
    * simplify!
    
    * 4
    
    * ;osu;osu;osu
    
    * Allowing searching for video
    
    * Move to discord.py rewrite
    
    * Update req and bot.py
    
    * rewrite final changes
    
    had to remove music functionality
    
    * hmm
    
    * git
    
    * ;L
    
    * koko
    
    * git://github.com/Rapptz/discord.py@rewrite#egg=discord.py[voice]
    
    * fix send file
    
    * fix voice client
    
    * Add search functionality
    
    * Experimentation
    
    * LOL i think this is enough

--- ext/voice.py
+++ ext/voice.py
@@ -10,56 +10,32 @@ import random
 class voice:
     def __init__(self, client):
         self.client = client
-        self.players = {}
         self.session = aiohttp.ClientSession()
+        self.voiceCs = {}
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def join(self, ctx):
-        self.channel = ctx.message.author.voice.voice_channel
-        await self.client.join_voice_channel(self.channel)
+        self.voiceCs[ctx.guild.id] = await ctx.message.author.voice.channel.connect()
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def leave(self, ctx):
-        self.server = ctx.message.server
-        self.voice_client = self.client.voice_client_in(self.server)
-        await self.voice_client.disconnect()
+        await self.voiceCs[ctx.guild.id].disconnect()
 
 #---------------------------------------------YOUTUBE---------------------------------------------------------------------------------
-    @commands.command(pass_context=True)
-    async def play(self, ctx, url):
-        self.server = ctx.message.server
-        self.voice_client = self.client.voice_client_in(self.server)
-        if self.voice_client is None:
-            self.voice_client = await self.client.join_voice_channel(ctx.message.author.voice.voice_channel)    
-        self.player = await self.voice_client.create_ytdl_player(url)
-        self.players[self.server.id] = self.player
-        self.player.start()
-    
-    @commands.command(pass_context=True)
-    async def pause(self, ctx):
-        self.players[ctx.message.server.id].pause()
-
-    @commands.command(pass_context=True)
-    async def resume(self, ctx):
-        self.players[ctx.message.server.id].resume()
-
-    @commands.command(pass_context=True)
-    async def stop(self, ctx):
-        self.players[ctx.message.server.id].stop()
-
-    @commands.command(pass_context=True)
+    @commands.command()
     async def ytdl(self, ctx, url, ext="mp3"):
-        msg = await self.client.say("Nep is trying her hardest to get your file. https://i.kym-cdn.com/photos/images/original/001/283/141/58e.gif")
+        msg = await ctx.send("Nep is trying her hardest to get your file. https://i.kym-cdn.com/photos/images/original/001/283/141/58e.gif")
         name = random.getrandbits(64)
         if ext == "mp4":
-            process = await asyncio.create_subprocess_shell("youtube-dl --default-search \"ytsearch\" -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 -o {}.mp4 {}".format(name, url), stdout=asyncio.subprocess.PIPE)
+            process = await asyncio.create_subprocess_shell("youtube-dl --no-playlist --default-search \"ytsearch\" -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 -o {}.mp4 {}".format(name, url), stdout=asyncio.subprocess.PIPE)
             await process.communicate()
         else:
             ext = "mp3"
-            process = await asyncio.create_subprocess_shell("youtube-dl --default-search \"ytsearch\" --embed-thumbnail --audio-quality 0 --extract-audio --audio-format mp3 -o {}.mp3 {}".format(name,url), stdout=asyncio.subprocess.PIPE)
+            process = await asyncio.create_subprocess_shell("youtube-dl --no-playlist --default-search \"ytsearch\" --embed-thumbnail --audio-quality 0 --extract-audio --audio-format mp3 -o {}.mp3 {}".format(name,url), stdout=asyncio.subprocess.PIPE)
             await process.communicate()
         await database.sendFile(self, ctx, name, ext)
-        await self.client.delete_message(msg)
+        os.remove(f"{name}.{ext}")
+        await msg.delete()
 
 
 
