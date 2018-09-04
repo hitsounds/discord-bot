a47fea714b5497e4de7ff7e07203cbb0906d8edb
Marvy <marvelrenju1@gmail.com>
2018-09-04 21:54:46 +0100

    Flair

--- bot.py
+++ bot.py
@@ -1,14 +1,18 @@
 import discord
 from discord.ext import commands
 import os
+import asyncio
+import random
+
 
 TOKEN = os.environ.get('TOKEN')
-
 cogs_dir = "ext"
 i_cogs = []
+status_messages = ["Tea Party with Abijith", "the piano", "UP", ";help", "Osu!", "without limits", "hard mode"]
 
-client = commands.Bot(command_prefix = ";")
+client = commands.Bot(command_prefix = [";","<@!476383348969963531>"])
 client.remove_command("help")
+
 
 @client.event
 async def on_ready():
@@ -31,6 +35,13 @@ async def help(ctx):
     await ctx.message.delete()
     await ctx.send("https://goo.gl/vya4Sp")
 
+async def status_msg():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        await asyncio.sleep(900)
+        await client.change_presence(activity=discord.Game(name=random.choice(status_messages))
+
+
 
 
 #loading the extensions from ext/ folder
@@ -43,4 +54,5 @@ for extension in [f.replace('.py', '') for f in os.listdir(cogs_dir) if os.path.
             print(f'Failed to load extension {extension}.') 
             print (e)
 
+client.loop.create_task(status_msg())
 client.run(TOKEN)
