512ac5a3b6b721ba973055a0aba5b8edcd379322
Marvy <marvelrenju1@gmail.com>
2018-09-07 16:34:09 +0100

    owb

--- ext/voice.py
+++ ext/voice.py
@@ -8,68 +8,13 @@ from ext.database import database
 import random
 import functools
 
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-}
-
-ffmpegopts = {
-    'before_options': '-nostdin',
-    'options': '-vn'
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
-class YTDLSource(discord.PCMVolumeTransformer):
-
-    def __init__(self, source, *, data, requester):
-        super().__init__(source)
-        self.requester = requester
-        self.title = data.get('title')
-        self.web_url = data.get('webpage_url')
-
-    def __getitem__(self, item: str):
-        return self.__getattribute__(item)
-
-    @classmethod
-    async def create_source(cls, ctx, search: str, *, loop, download=False):
-        loop = loop or asyncio.get_event_loop()
-        to_run = functools.partial(ytdl.extract_info, url=search, download=download)
-        data = await loop.run_in_executor(None, to_run)
-
-        if 'entries' in data:
-            data = data['entries'][0]
-        if download:
-            source = ytdl.prepare_filename(data)
-        else:
-            return {'webpage_url': data['webpage_url'], 'requester': ctx.author, 'title': data['title']}
-        return cls(discord.FFmpegPCMAudio(source), data=data, requester=ctx.author)
-
-    @classmethod
-    async def regather_stream(cls, data, *, loop):
-        loop = loop or asyncio.get_event_loop()
-        requester = data['requester']
-
-        to_run = functools.partial(ytdl.extract_info, url=data['webpage_url'], download=False)
-        data = await loop.run_in_executor(None, to_run)
-
-        return cls(discord.FFmpegPCMAudio(data['url']), data=data, requester=requester)
-
 
 
 class voice:
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
+"""    @commands.command()
     async def join(self, ctx):
         if ctx.voice_client is not None:
             return await ctx.voice_client.move_to(ctx.message.author.voice.channel)
@@ -95,7 +40,7 @@ class voice:
             else:
                 await ctx.send("You are not connected to a voice channel.")
         elif ctx.voice_client.is_playing():
-            ctx.voice_client.stop()
+            ctx.voice_client.stop()"""
 
 
 #---------------------------------------------YOUTUBE---------------------------------------------------------------------------------
