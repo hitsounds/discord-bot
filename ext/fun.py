2aa77385ddbdf7d874f59975b3cc15e40287b69d
Marvy <marvelrenju1@gmail.com>
2018-09-22 14:27:09 +0100

    probs will break something

--- ext/fun.py
+++ ext/fun.py
@@ -21,8 +21,8 @@ class fun:
     def __init__(self, client):
         self.client = client
         self.reddit = praw.Reddit(client_id=os.environ.get('C_ID'), client_secret=os.environ.get('C_S'), user_agent='bot.py A discord bot | https://github.com/Hitsounds/discord-bot')
-        self.osuAPIkey = os.environ.get('OSU_KEY')
-        
+
+
     @commands.command(name='anime', aliases=['manga'])
     async def kitsu_search(self, ctx, *, search: str):
         async with ctx.message.channel.typing():
@@ -42,9 +42,6 @@ class fun:
             await ctx.send(embed=embed)
 
 
-
-
-
     @commands.group()
     async def bws(self, ctx):
         if ctx.invoked_subcommand is None:
@@ -52,7 +49,8 @@ class fun:
             for i in range(0,random.randint(1, 10)):
                 submission = next(x for x in bwl if not x.stickied)
             await ctx.send(submission.url)
-    
+
+
     @bws.command()
     async def dump(self, ctx):
         sreddit = self.reddit.subreddit('awwnime')
@@ -63,7 +61,8 @@ class fun:
         for i in range(0, 10):
             embed.add_field(name="#{}".format(i+1), value="[{url}]({url})".format(url = next(x for x in bwl if not x.stickied).url), inline=False)
         await ctx.send(embed=embed)
-    
+
+
     @commands.command()
     async def yomama(self, ctx):
         await ctx.message.delete()
@@ -71,6 +70,7 @@ class fun:
             resp = await session.get("http://api.yomomma.info/")
         data = await resp.json()
         await ctx.send(data["joke"])
+
 
     @commands.command()
     async def banter(self, ctx):
@@ -80,70 +80,10 @@ class fun:
             lol = await resp.text()
             embed=discord.Embed(title="OwO", description=random.choice(lol.split("\n")), color=0x0a94e7)
             embed.set_footer(text = "Credit to George's dead banter bot", icon_url = "https://cdn.discordapp.com/avatars/478220076068241408/8560a1bedb1432d1cdf8dcf634ac3a4d.png")
-            await ctx.send(embed=embed)
+            await ctx.send(embed=embed)       
 
 
-
-    
-
-
-
-
-    @commands.command()
-    async def osu(self, ctx, *args):
-        if len(args)>0 and args[0] == "set":
-            """
-            Set osu name
-            
-            """
-            if len(args) < 2:
-                msg = await ctx.send("Pass a osu! user name or id with the command")
-                asyncio.sleep(2)
-            else:
-                async with aiohttp.ClientSession() as session:
-                    dtls = await session.get("https://osu.ppy.sh/api/get_user?k={key}&u={name}&m=0".format(key = self.osuAPIkey, name = args[1]))
-                dtls = await dtls.json()
-                await database.query("""UPDATE users SET osu_id='{osuid}' WHERE user_id={userID}""".format(userID = ctx.message.author.id, osuid = dtls[0]["user_id"]))
-                msg = await ctx.send("Osu! linked", delete_after=5.0)
-                await ctx.message.delete()
-        else:
-            """
-            
-            Get osu stats
-            
-            """
-            await ctx.trigger_typing()
-            async with aiohttp.ClientSession() as session:
-                if len(args) == 0:
-                    arg = await database.query(f"SELECT osu_id FROM users WHERE user_id={ctx.message.author.id}")
-                    arg = arg[0]
-                    dtls = await session.get("https://osu.ppy.sh/api/get_user?k={key}&u={name}&m=0".format(key = self.osuAPIkey, name = arg[0]))
-                else:
-                    dtls = await session.get("https://osu.ppy.sh/api/get_user?k={key}&u={name}&m=0".format(key = self.osuAPIkey, name = args[0]))
-            dtls = await dtls.json()
-            dtls = dtls[0]
-            embed=discord.Embed(title="Osu Stats" ,description="[Profile](https://osu.ppy.sh/u/{id}) | [PP+](https://syrin.me/pp+/u/{id}/) | [Skills](http://osuskills.tk/user/{name}) | [Osu!-chan](https://syrin.me/osuchan/u/{id}/?m=0) | [Osu!Track](https://ameobea.me/osutrack/user/{name})".format(name = dtls["username"],id = dtls["user_id"]), color=0xdc98a4)
-            embed.set_author(name="{} [{}]".format(dtls["username"], dtls["country"] ), url="https://osu.ppy.sh/u/{}".format(dtls["user_id"]), icon_url="https://a.ppy.sh/{}".format(dtls["user_id"]))
-            embed.set_thumbnail(url=r"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Osu%21Logo_%282015%29.png/220px-Osu%21Logo_%282015%29.png")
-            embed.add_field(name="PP", value=dtls["pp_raw"], inline=True)#
-            embed.add_field(name="Accuracy", value=str(round(float(dtls["accuracy"]),2))+"%", inline=True)
-            embed.add_field(name="Level", value=dtls["level"], inline=True)
-            embed.add_field(name="Playcount", value=dtls["playcount"], inline=True)
-            embed.add_field(name="Global Rank", value="#" + dtls["pp_rank"], inline=True)
-            embed.add_field(name=dtls["country"]+" Rank", value="#"+ dtls["pp_country_rank"], inline=True)
-            await ctx.send(embed=embed)
-
-
-            
-
-
-
-
-
-
-
-
-    @commands.command(name='ping', aliases=['pang',"pong"])
+    @commands.command(name='ping', aliases=['pang',"pong","pung"])
     async def ping_(self, ctx):
         template = random.choice(list(ping_formats))
         tempdDetails = ping_formats[template]
@@ -157,6 +97,9 @@ class fun:
         with open(f"ping_out_{ctx.message.author.name}.png", "rb") as f:
             await database.sendFile(self, ctx, f)
         os.remove(f"ping_out_{ctx.message.author.name}.png")
+
+
+
 
 
 
