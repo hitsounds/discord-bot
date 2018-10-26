4487076921a7b068b4013106c5b6d66b95e21e26
Marvy <marvelrenju1@gmail.com>
2018-10-26 15:45:16 +0100

    fix

--- ext/music.py
+++ ext/music.py
@@ -275,8 +275,7 @@ class Music:
             await ctx.invoke(self.connect_)
         player = self.get_player(ctx)
         plist = await database.query(f"SELECT s_playlist FROM users WHERE user_id={ctx.message.author.id}")
-        print(plist)
-        plist = plist[0]
+        plist = plist[0][0]
         for m in plist:
             player.queue.put(m)
 
