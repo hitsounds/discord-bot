ec501ba04e9ae198e15beabcc62c65833d5f3302
Marvy <marvelrenju1@gmail.com>
2019-01-04 18:36:58 +0000

    await it

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ async def status_msg():
 async def update_youtube_dl():
     await client.wait_until_ready()
     while True:
-        asyncio.create_subprocess_shell("sudo pip3 install -e git://github.com/rg3/youtube-dl@d7c3af7a72a1e6bd7f93321cce4daf2a13ebdf12#egg=youtube_dl --upgrade") 
+        await asyncio.create_subprocess_shell("sudo pip3 install -e git://github.com/rg3/youtube-dl@d7c3af7a72a1e6bd7f93321cce4daf2a13ebdf12#egg=youtube_dl --upgrade") 
         await process.communicate()
         await client.get_channel(426816759648092160).send("Ytdl updated")
         await asyncio.sleep(86400)
