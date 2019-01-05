8b244466297b8c8c670ca7a2e782ad7f11e0daf7
Marvy <marvelrenju1@gmail.com>
2019-01-05 17:35:17 +0000

    Handle discord oauth grant on web server instead

--- ext/auth.py
+++ ext/auth.py
@@ -21,30 +21,7 @@ class discord_auth:
     
     @auth.command()
     async def discord(self, ctx):
-        await ctx.message.author.send(r"<https://discordapp.com/api/oauth2/authorize?client_id=476383348969963531&redirect_uri=https%3A%2F%2Fdicsordbot.herokuapp.com&response_type=code&scope=identify%20email%20connections%20guilds%20guilds.join>")
-        """await ctx.message.author.send("Click the above link. Authorise the bot and then send the redirect link to me! This will timeout in 30 secs.")
-        def check(m):
-            return m.content.startswith("https://dicsordbot.herokuapp.com/?code=") or m.content.startswith("dicsordbot.herokuapp.com/?code=")
-        try:
-            msg = await self.client.wait_for('message', check=check, timeout=60.0)
-        except asyncio.TimeoutError:
-            await ctx.message.author.send("I'm done waiting.")
-        else:
-            code = msg.content.split("?code=")[1]
-            print(code)
-            data = {
-                'client_id': self.client.user.id,
-                'client_secret': self.cs,
-                'grant_type': 'authorization_code',
-                'code': code,
-                'redirect_uri': "https://dicsordbot.herokuapp.com",
-                'scope': 'connections identify email guilds guilds.join'
-            }
-            async with aiohttp.ClientSession() as session:
-                resp = await session.post("https://discordapp.com/api/oauth2/token", params=data)
-                resp = await resp.json()
-                print(resp)"""
-                
+        await ctx.message.author.send("https://api.hitsounds.moe/discord/login")
 
 
 
