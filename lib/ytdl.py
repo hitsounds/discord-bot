6e00d6d07511d0f29553ef6bf7eaddd259b453aa
Marvy <marvelrenju1@gmail.com>
2019-03-18 20:12:09 +0000

    Only download audio
    Previously downloading audio + video, merging then extracting. Painfully inefficient.

--- lib/ytdl.py
+++ lib/ytdl.py
@@ -12,6 +12,7 @@ class ytdl_downloader():
 		"mp3": {
 			'default_search': 'auto',
 			"verbose": True,
+			"format":"bestaudio/best",
 			"postprocessor_args": ["-movflags", "faststart"],
 			"writethumbnail": True,
 			"noplaylist": True,
