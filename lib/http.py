f6b12282deed21dd95d2868f24940fad57b48b95
Marvy <marvelrenju1@gmail.com>
2019-03-19 16:48:19 +0000

    changes

--- lib/http.py
+++ lib/http.py
@@ -11,9 +11,9 @@ async def file_from_url(url):
 
 
 async def sendfile(fileobj, *, d_ctx=None, filename=None, spoiler=False):
-	if os.fstat(fileobj.fileno()).st_size/1048576. < 7 and d_ctx != None:
+	if os.fstat(fileobj.fileno()).st_size/1048576. < 7.9 and d_ctx != None:
 		return await d_ctx.send(file=discord.File(fileobj))
-	elif os.fstat(fileobj.fileno()).st_size/1048576. < 510.:
+	elif os.fstat(fileobj.fileno()).st_size/1048576. < 40.:
 		async with aiohttp.ClientSession() as session:
 			resp = await session.post("https://0x0.st", data={"file": fileobj})
 		return await resp.text()
